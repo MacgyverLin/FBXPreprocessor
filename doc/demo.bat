edce40d581d0ff1d9bb3565ba56e21e02c001b33
guanronlin <guanronlin@tencent.com>
2021-09-22 14:22:27 +0800

    add model

---
+++ doc/demo.bat
@@ -0,0 +1,7 @@
+FBXPreprocessor 1.fbx -o 1out.fbx -engine unity -cts 1 -sc 1
+FBXPreprocessor 2.fbx -o 2out.fbx -engine unity -cts 1 -sc 1
+FBXPreprocessor 3.fbx -o 3out.fbx -engine unity -cts 1 -sc 1
+FBXPreprocessor 4.fbx -o 4out.fbx -engine unity -cts 1 -sc 1
+FBXPreprocessor 5.fbx -o 5out.fbx -engine unity -cts 1 -sc 1
+FBXPreprocessor 6.fbx -o 6out.fbx -engine unity -cts 1 -sc 1
+FBXPreprocessor 7.fbx -o 7out.fbx -engine unity -cts 1 -sc 1
