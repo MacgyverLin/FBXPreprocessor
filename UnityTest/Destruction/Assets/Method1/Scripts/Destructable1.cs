95c15162170b6cdf63ac24e9ed0534a133f4ac9d
guanronlin <guanronlin@tencent.com>
2021-09-26 12:26:01 +0800

    tidyupcode

--- UnityTest/Destruction/Assets/Method1/Scripts/Destructable1.cs
+++ UnityTest/Destruction/Assets/Method1/Scripts/Destructable1.cs
@@ -2,30 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-class MiniRigidbody
+class PhysicsBase
 {
-    private Vector3 linearPosition = new Vector3();
-    private Vector3 linearVelocity = new Vector3();
-    private Vector3 linearAcc = new Vector3();
-    private float linearDrag = 0.1f;
+    protected Matrix4x4 transform = Matrix4x4.identity;
+    protected Vector4 translation = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+    protected Vector4 rotation = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+    protected Vector4 scale = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+    protected bool sleeping = false;
 
-    private Vector3 angularPosition = new Vector3();
-    private Vector3 angularVelocity = new Vector3();
-    private Vector3 angularAcc = new Vector3();
-    private float angularDrag = 0.1f;
-
-    private Matrix4x4 transform;
-    private Vector4 translation;
-    private Vector4 rotation;
-    private Vector4 scale;
-
-    bool sleeping;
-
-    public MiniRigidbody()
+    public PhysicsBase()
     {
     }
 
-    public void Init(Vector3 linearPosition, Vector3 linearVelocity, Vector3 linearAcc, float linearDrag,
+    virtual public void Update()
+    {
+    }
+
+    public bool IsSleeping()
+    {
+        return sleeping;
+    }
+
+    public Matrix4x4 GetTransform()
+    {
+        return transform;
+    }
+
+    public Vector4 GetTranslation()
+    {
+        return translation;
+    }
+
+    public Vector4 GetRotation()
+    {
+        return rotation;
+    }
+
+    public Vector4 GetScale()
+    {
+        return new Vector4(scale.x, scale.y, scale.z, 1.0f);
+    }
+};
+
+
+class SimplePhysics : PhysicsBase
+{
+    protected Vector3 linearPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    protected Vector3 linearVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+    protected Vector3 linearAcc = new Vector3(0.0f, 0.0f, 0.0f);
+    protected float linearDrag = 0.0f;
+    
+    protected Vector3 angularPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    protected Vector3 angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+    protected Vector3 angularAcc = new Vector3(0.0f, 0.0f, 0.0f);
+    protected float angularDrag = 0.0f;
+
+    public SimplePhysics()
+    {
+    }
+
+    public void Init(GameObject gameobject, Vector3 linearPosition, Vector3 linearVelocity, Vector3 linearAcc, float linearDrag,
                      Vector3 angularPosition, Vector3 angularVelocity, Vector3 angularAcc, float angularDrag,
                      Vector3 scale)
     {
@@ -54,12 +90,7 @@ class MiniRigidbody
         if (hit)
         {
             float fraction = hitInfo.distance / this.linearVelocity.magnitude;
-            Debug.Log(string.Format("Time:{0}, Distance:{1}, Mag:{2} ", fraction, hitInfo.distance, this.linearVelocity.magnitude));
-
-            this.linearDrag = 0.1f;
-            this.angularDrag = 0.1f;
-            this.linearAcc = Vector3.zero;
-            this.angularAcc = Vector3.zero;
+            // Debug.Log(string.Format("Time:{0}, Distance:{1}, Mag:{2} ", fraction, hitInfo.distance, this.linearVelocity.magnitude));
 
             ///////////////////////////////////////
             this.linearPosition += this.linearVelocity * dt * fraction;
@@ -69,7 +100,7 @@ class MiniRigidbody
             this.angularPosition += this.angularVelocity * dt * fraction;
             this.angularVelocity -= this.angularVelocity * this.angularDrag * dt;
 
-            this.linearPosition.y = hitInfo.point.y;
+            this.linearPosition = hitInfo.point;
 
             sleeping = true;
         }
@@ -86,11 +117,11 @@ class MiniRigidbody
             this.angularVelocity += this.angularAcc * dt;
         }
 
-        Quaternion q = new Quaternion();
-        q.eulerAngles = angularPosition;
-
         transform.SetTRS(this.linearPosition, q, scale);
         translation = this.linearPosition;
+
+        Quaternion q = new Quaternion();
+        q.eulerAngles = angularPosition;
         rotation = new Vector4(q.x, q.y, q.z, q.w);
     }
 
@@ -122,7 +153,7 @@ class MiniRigidbody
 
 public class Destructable1 : MonoBehaviour
 {
-    private MiniRigidbody[] miniRigidbodies = new MiniRigidbody[16];
+    private SimplePhysics[] physics = new SimplePhysics[16];
 
     // Start is void called before the first frame update
     void Start()
@@ -145,11 +176,12 @@ public class Destructable1 : MonoBehaviour
         float linearSpeed = 10.0f * explosionForce;
         float angularSpeed = 360.0f * explosionForce;
 
-        for (int i = 0; i < miniRigidbodies.Length; i++)
+        for (int i = 0; i < physics.Length; i++)
         {
-            miniRigidbodies[i] = new MiniRigidbody();
-            miniRigidbodies[i].Init
+            physics[i] = new SimplePhysics();
+            physics[i].Init
             (
+                this.gameObject,
                 this.transform.position,
                 new Vector3(Random.Range(-linearSpeed, linearSpeed), Random.Range(0.0f, linearSpeed * upwardsModifier), Random.Range(-linearSpeed, linearSpeed)),
                 new Vector3(0.0f, -9.8f, 0.0f),
@@ -167,9 +199,9 @@ public class Destructable1 : MonoBehaviour
         Vector4[] rotations = new Vector4[16];
         for (int i = 0; i < transforms.Length; i++)
         {
-            transforms[i] = miniRigidbodies[i].GetTransform();
-            translations[i] = miniRigidbodies[i].GetTranslation();
-            rotations[i] = miniRigidbodies[i].GetRotation();
+            transforms[i] = physics[i].GetTransform();
+            translations[i] = physics[i].GetTranslation();
+            rotations[i] = physics[i].GetRotation();
         }
 
         MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
@@ -188,13 +220,13 @@ public class Destructable1 : MonoBehaviour
         Matrix4x4[] transforms = new Matrix4x4[16];
         Vector4[] translations = new Vector4[16];
         Vector4[] rotations = new Vector4[16];
-        for (int i = 0; i < miniRigidbodies.Length; i++)
+        for (int i = 0; i < physics.Length; i++)
         {
-            miniRigidbodies[i].Update();
-            transforms[i] = miniRigidbodies[i].GetTransform();
+            physics[i].Update();
+            transforms[i] = physics[i].GetTransform();
 
-            translations[i] = miniRigidbodies[i].GetTranslation();
-            rotations[i] = miniRigidbodies[i].GetRotation();
+            translations[i] = physics[i].GetTranslation();
+            rotations[i] = physics[i].GetRotation();
         }
 
         MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
@@ -250,7 +282,7 @@ public class Destructable1 : MonoBehaviour
             UpdatePhysics();
 
             done = true;
-            foreach (var rb in miniRigidbodies)
+            foreach (var rb in physics)
             {
                 if (!rb.IsSleeping())
                 {
