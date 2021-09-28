38353ddde2839cd1ab362417b20af39110121cec
guanronlin <guanronlin@tencent.com>
2021-09-28 10:08:55 +0800

    demolishableData

---
+++ UnityTest/Destruction/Assets/Method1/Scripts/ModelImporterPostProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// Disable import of materials if the file contains
+// the @ sign marking it as an animation.
+public class ModelImporterPostProcessor : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        ModelImporter modelImporter = assetImporter as ModelImporter;
+        modelImporter.importAnimatedCustomProperties = true;
+
+        string jsonPath = modelImporter.assetPath.Replace(".fbx", ".json");
+        string scriptableObjectPath = modelImporter.assetPath.Replace(".fbx", ".asset");
+        //Debug.Log(jsonPath);
+        //Debug.Log(scriptableObjectPath);
+
+        TextAsset jsonText = AssetDatabase.LoadAssetAtPath<TextAsset>(jsonPath);
+        //Debug.Log(textAsset.text);
+
+        DemolishableData demolishableData = ScriptableObject.CreateInstance<DemolishableData>();
+        demolishableData.faceGroups.Add(new FaceGroup(0, new Bound(new Vector3(1, 2, 3), new Vector3(4, 5, 6))));
+        demolishableData.faceGroups.Add(new FaceGroup(1, new Bound(new Vector3(10, 20, 30), new Vector3(40, 50, 60))));
+        demolishableData.faceGroups.Add(new FaceGroup(1, new Bound(new Vector3(11, 21, 31), new Vector3(41, 51, 61))));
+
+        string text = JsonUtility.ToJson(demolishableData);
+        Debug.Log(text);
+        demolishableData.faceGroups.Clear();
+        JsonUtility.FromJsonOverwrite(text, demolishableData);
+
+        AssetDatabase.CreateAsset(demolishableData, jsonPath);
+        AssetDatabase.SaveAssets();
+        // AssetDatabase.DeleteAsset(jsonPath);
+    }
+}
